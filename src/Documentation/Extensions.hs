d7431d645e5e90fd3f8c4b1f3a32787320453665
min-nguyen <minhnguyen1995@googlemail.com>
2023-06-28 16:53:17 +0100

    derive2nd

--- src/Documentation/Extensions.hs
+++ src/Documentation/Extensions.hs
@@ -10,13 +10,13 @@ import Documentation.Forward
 import Documentation.Symbolic hiding (forwardAD_Dense)
 import Prelude hiding (Monoid)
 
-{-- | Second-order derivatives can be naively computed by applying symbolic differentiation twice, producing
+{-- | SECOND-ORDER DERIVATIVES can be naively computed by applying symbolic differentiation twice, producing
       a symbolic expression "(dy/dx)/dx :: Expr V", and then evaluating this at a concrete point.
         (Reminder: Symbolic diff uses "Var :: v -> Expr v" to evaluate variables into the symbolic semiring "Expr V"
         rather than into concrete numeric values.)
       The basic implementation is:
         derive2nd :: (Eq v, Semiring d) => (v -> d) -> Expr v -> Dense v (Dense v d)
-        derive2nd var e = Dense $ \x -> Dense $ \y ->
+        derive2nd var e = Dense $ λx -> Dense $ λy ->
           let Nagata _ (Dense sym_de)   = symbolic_Dense e
               dedx   = sym_de x          -- | Differentiate e wrt x
               Nagata _ (Dense sym_dedx) = symbolic_Dense dedx
@@ -24,13 +24,24 @@ import Prelude hiding (Monoid)
           in  eval var dedxdy            -- | Evaluate the symbolic derivative of dedxdy at point "var"
 --}
 
-{-- | SECOND-ORDER FORWARD-MODE AD specialises the Abstract AD to work with nested Nagata numbers "(D ⋉ (V -> D)) ⋉ (V -> (D ⋉ (V -> D)))".
+{-- | SECOND-ORDER DENSE FORWARD-MODE AD specialises the Abstract AD to use nested Nagata numbers (D ⋉ (V -> D)) ⋉ (V -> (D ⋉ (V -> D))):
+           Nagata (e, λx -> dedx) (λx -> (dedx, λy -> dedxdy))
       For a given expression e:
-        1. Their primals are themselves first-order dual numbers, (e, \x -> dedx) :: (D ⋉ (V -> D)), who tangents are functions to the first-order derivatives.
-        2. Their tangents are functions to second-order dual numbers, (\x -> (dedx, \y -> dedxdy)) :: (V -> (D ⋉ (V -> D))), whose tangents are in turn functions to second-order derivatives.
+        1. The primals are themselves first-order dual numbers:
+                 (e, λx -> dedx) :: (D ⋉ (V -> D))
+           whose primals are the result e, and tangents are functions to the first-order derivatives.
+        2. The tangents are functions to second-order dual numbers:
+                 (λx -> (dedx, λy -> dedxdy)) :: (V -> (D ⋉ (V -> D)))
+           whose primals are first-order derivatives, and tangents are functions to second-order derivatives.
+
+        .                 e                -- 0th order
+        .              /     \
+        .            e        dedx         -- 1st order
+        .          /   \     /    \
+        .         e  dedx  dedx  dedxdy    -- 2nd order
 --}
-derive2nd :: forall v d e. (Eq v, Kronecker v d e) => (v -> d) -> Expr v -> Dense v (Dense v d)
-derive2nd var e = Dense $ \x -> Dense $ \y ->
+forwardAD_Dense_2nd :: forall v d e. (Eq v, Kronecker v d e) => (v -> d) -> Expr v -> Dense v (Dense v d)
+forwardAD_Dense_2nd var e = Dense $ \x -> Dense $ \y ->
   let -- Define a generator that instantiates variables in Expr v to first-order, Nagata numbers (d ⋉ Dense v d).
       gen    :: v -> (d ⋉ Dense v d)
       gen z  = Nagata (var z) (delta z)
@@ -43,31 +54,38 @@ derive2nd var e = Dense $ \x -> Dense $ \y ->
       dedxdy = runDense dedx y
   in  dedxdy
 
-{--  | To avoid redundant recomputation of the same terms in Second-order AD above in (D ⋉ (V -> D)) ⋉ (V -> (D ⋉ (V -> D))):
+{--  | COMPACT SECOND-ORDER NAGATA NUMBERS (⋉⋉)
+       We define (⋉⋉) for the compact representation of Second-Order Nagata numbers (D ⋉ (V -> (D ⋉ (V -> D))):
+             Nagata2 e (λx -> (dedx, λy -> dedxdy))
+
         .                 e                -- 0th order
         .              /     \
         .            e        dedx         -- 1st order
-        .          /   \     /    \
-        .         e  dedx  dedx  dedxdy    -- 2nd order
-      We define the compact representation of Second-Order Nagata numbers (D ⋉ (V -> (D ⋉ (V -> D)))
+        .          /         /    \
+        .         e        dedx  dedxdy    -- 2nd order
 --}
-data v ⋉⋉ d = N2 d (Dense v (d ⋉ (Dense v d)))
+data v ⋉⋉ d = Nagata2 d (Dense v (d ⋉ (Dense v d)))
 
 instance (Semiring d) => Semiring (v ⋉⋉ d) where
-  zero        =  N2 zero mzero
-  one         =  N2 one  mzero
-  N2 f ddf ⊕ N2 g ddg =  N2 (f ⊕ g) (ddf ⊕ ddg)
-  N2 f ddf ⊗ N2 g ddg
-    =  N2 (f ⊗ g)
+  zero        =  Nagata2 zero mzero
+  one         =  Nagata2 one  mzero
+  Nagata2 f ddf ⊕ Nagata2 g ddg =  Nagata2 (f ⊕ g) (ddf ⊕ ddg)
+  Nagata2 f ddf ⊗ Nagata2 g ddg
+    =  Nagata2 (f ⊗ g)                                            -- Compute result (f * g)
           (Dense $ \x ->
-              let  Nagata dfdx ddfdx = runDense ddf x
-                   Nagata dgdx ddgdx = runDense ddg x
-              in   Nagata ((f ⊗ dgdx) ⊕ (g ⊗ dfdx))
+              let  Nagata dfdx ddfdx = runDense ddf x             -- Get first-order partial derivative df/dx
+                   Nagata dgdx ddgdx = runDense ddg x             -- Get first-order partial derivative dg/dx
+              in   Nagata ((dfdx ⊗ g) ⊕ (f ⊗ dgdx))               -- Compute first-order derivative d(f * g)/dx
                           (Dense $ \y ->
-                              let ddgdxdy = runDense ddgdx y
-                                  ddfdxdy = runDense ddfdx y
-                              in (ddfdxdy ⊗ g) ⊕ (f ⊗ ddgdxdy) ⊕ (dfdx ⊗ dgdx) ⊕ (dgdx ⊗ dfdx)))
+                              let ddfdxdy = runDense ddfdx y      -- Get second-order partial derivative df/dx/dy
+                                  ddgdxdy = runDense ddgdx y      -- Get second-order partial derivative dg/dx/dy
+                              in (ddfdxdy ⊗ g) ⊕ (f ⊗ ddgdxdy)    -- Compute second-order derivative d(f * g)/dx/dy
+                                 ⊕ (dfdx ⊗ dgdx) ⊕ (dgdx ⊗ dfdx)
+                          )
+          )
 
-forwardAD2nd_Dense :: Eq v => Semiring d => (v -> d) -> Expr v -> v ⋉⋉ d
-forwardAD2nd_Dense var = eval gen where
-  gen x = N2 (var x) (delta x)
+{-- |  COMPACT SECOND-ORDER DENSE AD is a variant of Abstract AD for Second-Order Nagata Numbers (⋉⋉)
+--}
+forwardAD_Dense_Compact2nd :: Eq v => Semiring d => (v -> d) -> Expr v -> v ⋉⋉ d
+forwardAD_Dense_Compact2nd var = eval gen where
+  gen x = Nagata2 (var x) (delta x)
