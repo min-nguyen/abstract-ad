c1fb3a5e05e2c5c19e9c7d872a6b090d66381bb6
min-nguyen <minhnguyen1995@googlemail.com>
2023-06-28 12:03:47 +0100

    derive2nd

--- src/Documentation/Extensions.hs
+++ src/Documentation/Extensions.hs
@@ -6,15 +6,18 @@ module Documentation.Extensions where
 import Documentation.AbstractAD
 import Documentation.Forward
 
-{-- | Symbolic AD is simply Forward-mode AD, the dual number Semiring d => (d, v -> d),
-      but where we use "Var :: v -> Expr v" to evaluate variables into the symbolic
-      semiring "Expr V" rather than into concrete numeric values.
+{-- | Second-order derivatives can be naively computed by applying symbolic differentiation twice, producing
+      a symbolic expression "d2y/dx2 :: Expr V", and then evaluating this at a concrete point.
+        (Reminder: Symbolic diff uses "Var :: v -> Expr v" to evaluate variables into the symbolic semiring "Expr V"
+        rather than into concrete numeric values.)
 --}
-symbolicAD :: (Eq v, Semiring d) => Expr v -> Expr v ⋉ (Dense v (Expr v))
-symbolicAD = abstractAD Var
+symbolic_Dense :: Eq v => Expr v -> Expr v ⋉ (Dense v (Expr v))
+symbolic_Dense = abstractAD Var
 
 derive2nd :: (Eq v, Semiring d) => (v -> d) -> Expr v -> Dense v (Dense v d)
-derive2nd var e =
-  let f = forwardAD_Dense var
-
-  in undefined -- fmap (fmap (eval var) . tanN . forwardADDense Var) . tanN .
+derive2nd var e = Dense $ \x  -> Dense $ \y ->
+  let Nagata _ (Dense sym_de)   = symbolic_Dense e
+      dedx   = sym_de x          -- | Differentiate e wrt x
+      Nagata _ (Dense sym_dedx) = symbolic_Dense dedx
+      dedxdy = sym_dedx y        -- | Differentiate e wrt x wrt y
+  in  eval var dedxdy            -- | Evaluate the symbolic derivative of dedxdy at point "var"
